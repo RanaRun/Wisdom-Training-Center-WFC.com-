6e6c7b3d51575ea070b10c13727f1b464c6593a9
RanaRun <Ranarun2025@gmail.com>
2025-07-07 23:23:29 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,11 +1,13 @@
- // data.js
+// data.js
 
 const studentsDB = [
     { id: '909090', name: 'កែវ សុវណ្ណបូរមី', enrolledCourses: [{ courseId: 1 }, { courseId: 2 }] },
-    { id: '123456', name: 'សុខ ចាន់ថា', enrolledCourses: [{}courseId: 1] }
+    { id: '123456', name: 'សុខ ចាន់ថា', enrolledCourses: [{ courseId: 2}] },
+    // បន្ថែមសិស្សថ្មីនៅទីនេះ
+    { id: '112233', name: 'កែវ សុវណ្ណបូមី', enrolledCourses: [{ courseId: 2 }] } // សន្មតថាគាត់បានចុះឈ្មោះរៀនវគ្គដែលគាត់មានវិញ្ញាបនបត្រ
 ];
 
 const verifiedCertsDB = [
     { studentName: 'កែវ សុវណ្ណបូរមី', courseName: 'វេយ្យាករណ៍ និងតែងសេចក្ដី', completionDate: '2025-06-15', verificationCode: 'MBP-06-2025' },
-	 { studentName: 'សុខ ចាន់ថា', courseName: 'វេយ្យាករណ៍ និងតែងសេចក្ដី', completionDate: '2025-06-15', verificationCode: 'MBP-07-2025' }
+	{ studentName: 'កែវ សុវណ្ណ<strong>បូមី</strong>', courseName: 'វេយ្យាករណ៍ និងតែងសេចក្ដី', completionDate: '2025-06-15', verificationCode: 'MBP-07-2025' }
 ];
