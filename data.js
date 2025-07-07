3cd82bb7264ccb370fc81df7a22d236277a00d73
RanaRun <Ranarun2025@gmail.com>
2025-07-07 23:07:36 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,11 @@
+ // data.js
+
+const studentsDB = [
+    { id: '909090', name: 'កែវ សុវណ្ណបូរមី', enrolledCourses: [{ courseId: 1 }, { courseId: 2 }] },
+    { id: '123456', name: 'សុខ ចាន់ថា', enrolledCourses: [{}courseId: 1] }
+];
+
+const verifiedCertsDB = [
+    { studentName: 'កែវ សុវណ្ណបូរមី', courseName: 'វេយ្យាករណ៍ និងតែងសេចក្ដី', completionDate: '2025-06-15', verificationCode: 'MBP-06-2025' },
+	 { studentName: 'កែវ សុវណ្ណបូមី', courseName: 'វេយ្យាករណ៍ និងតែងសេចក្ដី', completionDate: '2025-06-15', verificationCode: 'MBP-07-2025' }
+];
