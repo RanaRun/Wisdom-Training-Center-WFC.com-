aee9441bdd3bde5232c080ec5ee3710d6a5636aa
RanaRun <Ranarun2025@gmail.com>
2025-07-10 16:57:22 +0700

    Update data.js

--- data.js
+++ data.js
@@ -2,7 +2,7 @@
 
 const studentsDB = [
     { id: '110725', name: 'លី ស្រីនាង', enrolledCourses: [{ courseId: 3  }] },
-    { id: '123456', name: 'សុខ ចាន់ថា', enrolledCourses: [{ courseId: 2}] },
+    { id: '120725', name: 'វឿន សៃហ៊ាក់', enrolledCourses: [{ courseId: 3}] },
     // បន្ថែមសិស្សថ្មីនៅទីនេះ
     { id: '112233', name: 'កែវ សុវណ្ណបូមី', enrolledCourses: [{ courseId: 2 }] } // សន្មតថាគាត់បានចុះឈ្មោះរៀនវគ្គដែលគាត់មានវិញ្ញាបនបត្រ
 ];
